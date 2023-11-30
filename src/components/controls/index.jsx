f92217df1fd0fa4ce6e181c8f81c88b699b62064
AlexandrKataev <dkeyman@mail.ru>
2023-11-30 23:12:05 +0300

    styles refactoring

--- src/components/controls/index.jsx
+++ src/components/controls/index.jsx
@@ -1,33 +1,46 @@
 import React from "react";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
-import './style.css';
+import "./style.css";
 import { plural } from "../../utils";
 
-
-function Controls({onOpenCart, cartItemCount, totalPrice}) {
+function Controls({ onOpenCart, cartItemCount, totalPrice }) {
   return (
-    <div className='Controls'>
-			<div>В корзине: 
-				{cartItemCount > 0 
-				? <b>{' ' + cartItemCount + ' ' + plural(cartItemCount, {one: 'товар', few: 'товара', many: 'товаров'}) + ' / ' + totalPrice + ' ₽'}</b> 
-				: <b> пусто</b>} 
-			</div>
+    <div className="Controls">
+      <div>В корзине:</div>
+      <div>
+        {cartItemCount > 0 ? (
+          <b>
+            {cartItemCount +
+              " " +
+              plural(cartItemCount, {
+                one: "товар",
+                few: "товара",
+                many: "товаров",
+              }) +
+              " / " +
+              totalPrice +
+              " ₽"}
+          </b>
+        ) : (
+          <b> пусто</b>
+        )}
+      </div>
       <button onClick={onOpenCart}>Перейти</button>
     </div>
-  )
+  );
 }
 
 Controls.propTypes = {
   onOpenCart: PropTypes.func,
-	cartItemCount: PropTypes.number,
-	totalPrice: PropTypes.number
+  cartItemCount: PropTypes.number,
+  totalPrice: PropTypes.number,
 };
 
 Controls.defaultProps = {
   onOpenCart: () => {},
-	cartItemCount: 0,
-	totalPrice: 0
-}
+  cartItemCount: 0,
+  totalPrice: 0,
+};
 
 export default React.memo(Controls);
--- src/store.js
+++ src/store.js
@@ -1,4 +1,4 @@
-import {generateCode} from "./utils";
+import { generateCode } from "./utils";
 
 /**
  * Хранилище состояния приложения
@@ -18,8 +18,8 @@ class Store {
     this.listeners.push(listener);
     // Возвращается функция для удаления добавленного слушателя
     return () => {
-      this.listeners = this.listeners.filter(item => item !== listener);
-    }
+      this.listeners = this.listeners.filter((item) => item !== listener);
+    };
   }
 
   /**
@@ -46,9 +46,12 @@ class Store {
   addItem() {
     this.setState({
       ...this.state,
-      list: [...this.state.list, {code: generateCode(), title: 'Новая запись'}]
-    })
-  };
+      list: [
+        ...this.state.list,
+        { code: generateCode(), title: "Новая запись" },
+      ],
+    });
+  }
 
   /**
    * Удаление записи по коду
@@ -58,47 +61,52 @@ class Store {
     this.setState({
       ...this.state,
       // Новый список, в котором не будет удаляемой записи
-      list: this.state.list.filter(item => item.code !== code)
-    })
-  };
+      list: this.state.list.filter((item) => item.code !== code),
+    });
+  }
 
-	/**
-	 * Добавление товара в корзину
-	 * @param item
-	 */
-  addToCart(item) {
-		if(this.state.cartList.some(el => el.code === item.code)) {
-			this.setState({...this.state, cartList: this.state.cartList.map((el) => {
-				if(el.code === item.code) {
-					return {...el, count: el.count + 1}
-				} else {
-					return el
-				}
-			})})
-		} else {
-			this.setState({...this.state, cartList: [...this.state.cartList, {...item, count: 1}]})
-		}
-	}
-
-	/**
-	 * Удаление товара из корзины
-	 * @param code
-	 */
-	deleteFromCart(code) {
-		console.log('удалить товар')
-		this.setState({
-      ...this.state,
-      cartList: this.state.cartList.filter(item => item.code !== code)
-    })
-	}
   /**
-	 * Получить итоговую сумму
-	 */
-	getTotalPrice() {
-		return this.state.cartList.reduce((sum, item) => {
-			return sum + item.price * item.count
-		}, 0)
-	}
+   * Добавление товара в корзину
+   * @param item
+   */
+  addToCart(item) {
+    if (this.state.cartList.some((el) => el.code === item.code)) {
+      this.setState({
+        ...this.state,
+        cartList: this.state.cartList.map((el) => {
+          if (el.code === item.code) {
+            return { ...el, count: el.count + 1 };
+          } else {
+            return el;
+          }
+        }),
+      });
+    } else {
+      this.setState({
+        ...this.state,
+        cartList: [...this.state.cartList, { ...item, count: 1 }],
+      });
+    }
+  }
+
+  /**
+   * Удаление товара из корзины
+   * @param code
+   */
+  deleteFromCart(code) {
+    this.setState({
+      ...this.state,
+      cartList: this.state.cartList.filter((item) => item.code !== code),
+    });
+  }
+  /**
+   * Получить итоговую сумму
+   */
+  getTotalPrice() {
+    return this.state.cartList.reduce((sum, item) => {
+      return sum + item.price * item.count;
+    }, 0);
+  }
 }
 
 export default Store;
